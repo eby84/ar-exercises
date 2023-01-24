81dafb36629e77fe3f8c89b135d154bbf23357ac
Ebele <ebele.omanukwue@telus.com>
2023-01-23 20:53:44 -0700

    Exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -20,3 +20,24 @@ puts "----------"
 #Loop through each of these stores and output their name and annual revenue on each line.
 
 #Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+
+store_4 = Store.create(name: "Surrey", annual_revenue: 2240000, mens_apparel: false, womens_apparel: true)
+
+store_5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store_6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+
+
+@mens_stores = Store.where(mens_apparel: true)
+
+Store.all.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+  
+end
+
+@womens_stores = Store.where("womens_apparel = true", "annual_revenue < 1000000") 
+
+puts @mens_stores
+puts @womens_stores
